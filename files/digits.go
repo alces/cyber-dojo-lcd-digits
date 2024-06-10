2f6a461268efff7d6426277af426471bb48a13a9
c6G3fb <c6G3fb@cyber-dojo.org>
2024-06-10 08:38:33 +0000

    3 ran tests, predicted green, got green

--- files/digits.go
+++ files/digits.go
@@ -1,14 +1,16 @@
 package lcd
 
 func digit(num int) [3]string {
+    var result [3]string
+
     switch(num) {
     case 0:
-        return [3]string{
+        result = [3]string{
             " _ ",
             "| |",
             "|_|",
         }
-    default:
-        return [3]string{}
     }
+    
+    return result
 }
