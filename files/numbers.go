0863ed88acc1dd5756070d4cb5d373a1fed46a6d
c6G3fb <c6G3fb@cyber-dojo.org>
2024-06-11 13:44:30 +0000

    44 ran tests, predicted green, got green

--- files/numbers.go
+++ files/numbers.go
@@ -27,12 +27,11 @@ func chopTail(number int) (head, tail int) {
     return number/10, number%10
 }
 
-func splitNumber(number int) (result []int) {
-    head := number
-    
-    for head > 0 {
-        head, tail := chopTail(head)
+func splitNumber(number int) (result []int) {    
+    for number > 0 {
+        head, tail := chopTail(number)
         result = append(result, tail)
+        number = head
     }
     
     return
